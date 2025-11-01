34cd5c235c30b9f6d021da4bbf662360d94f1bf0
Zero-wyc <Zero-wyc@users.noreply.github.com>
2025-11-01 17:18:12 +0000

    deploy: 22be0af0597f6f396efba68d410eeaadc252c64f

---
+++ assets/back.DsRip4Sz.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.DBH2nt6U.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
