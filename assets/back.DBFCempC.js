eacd99f183e8dc0f4a375cb87fc814d723525bf9
Zero-wyc <Zero-wyc@users.noreply.github.com>
2025-12-27 16:31:08 +0000

    deploy: 197eda9ee0f9d101f310e7a84de19e7007977de6

---
+++ assets/back.DBFCempC.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.CRhiQbjs.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
